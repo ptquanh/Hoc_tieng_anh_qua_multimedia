13463457e71c61ac3d2d6b7e8cf624bdd4cf47eb
ptquanh <vocsiquocanh@gmail.com>
2024-12-05 15:50:25 +0700

    create react app, install scss

--- Backend/src/routes/web.js
+++ Backend/src/routes/web.js
@@ -1,10 +1,11 @@
 import express from "express";
+import homeController from "../controller/homeController"
 
 const router = express.Router();
+
 const initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello World");
-  });
+  router.get("/", homeController.handleHome);
+  router.get("/user", homeController.handleUserPage);
   return app.use("/", router);
 };
 
