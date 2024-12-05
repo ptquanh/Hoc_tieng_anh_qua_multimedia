73c5e0d38de072b8dcfe20d9f560e68b370e70db
ptquanh <vocsiquocanh@gmail.com>
2024-12-05 18:09:37 +0700

    install bootstrap, update route

---
+++ Frontend/src/components/Login/Login.js
@@ -0,0 +1,9 @@
+const Login = (props) => {
+  return (
+    <div className="login-container">
+      login
+      <button className="btn btn-primary">test</button>
+    </div>
+  );
+};
+export default Login;
