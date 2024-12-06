cefec08c091a56b1b81ddd7d9014ce4bac663d01
ptquanh <vocsiquocanh@gmail.com>
2024-12-06 09:44:33 +0700

    c with database

--- Backend/src/controller/homeController.js
+++ Backend/src/controller/homeController.js
@@ -1,10 +1,31 @@
+import mysql from "mysql2";
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "studyenglish",
+});
 const handleHome = (req, res) => {
-  return res.render("home.ejs")
+  return res.render("home.ejs");
 };
 const handleUserPage = (req, res) => {
   return res.render("user.ejs");
 };
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.username;
+  connection.query(
+    "INSERT INTO users (email, password, username) VALUES (?, ?, ?)",
+    [email, password, username],
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+  );
+  return res.send("handleCreateNewUser");
+};
 module.exports = {
   handleHome,
   handleUserPage,
+  handleCreateNewUser,
 };
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
-require("dotenv").config()
+require("dotenv").config();
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 configViewEngine(app);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 initWebRoutes(app);
 
 app.listen(PORT, () => {
-  console.log("run succeed" + PORT);
+  console.log("run succeed " + PORT);
 });
